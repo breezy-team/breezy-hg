cd8e95336c45aa4d2a0f6508d92e19a095073afc
Jelmer Vernooij <jelmer@samba.org>
2009-09-07 15:16:43 +0200

    Docstrings.

--- branch.py
+++ branch.py
@@ -15,14 +15,17 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+"""Mercurial Branch support."""
+
 import mercurial.node
 import os
 
 from bzrlib.branch import (
     BranchCheckResult,
     BranchFormat,
-    PullResult,
+    BranchPushResult,
     InterBranch,
+    PullResult,
     )
 from bzrlib.decorators import (
     needs_read_lock,
--- ui.py
+++ ui.py
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-"""Implementation of the Mercurial UI interface that translates to Bazaar calls."""
+"""Mercurial UI interface implementation that translates to Bazaar calls."""
 
 from bzrlib import (
     config,

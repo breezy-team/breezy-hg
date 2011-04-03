03a801f278df92a7cfbe159a58c5ac60a1c7b37b
Jelmer Vernooij <jelmer@samba.org>
2011-04-03 22:35:43 +0200

    Add drevisions() test.

--- tests/test_push.py
+++ tests/test_push.py
@@ -17,12 +17,18 @@
 """Tests for pushing revisions into Mercurial repositories."""
 
 
-from bzrlib.tests import TestCase
+from bzrlib.tests import (
+    TestCase,
+    TestCaseWithTransport,
+    )
 
 from bzrlib.plugins.hg.changegroup import (
     chunkify,
+    drevisions,
     extract_base,
     )
+from bzrlib.plugins.hg.mapping import default_mapping
+from bzrlib.plugins.hg.overlay import get_overlay
 
 
 class ChunkifyTests(TestCase):
@@ -45,3 +51,20 @@ class ExtractBaseTests(TestCase):
         (entries, base) = extract_base(iter(["c", "a", "b"]))
         self.assertEquals("c", base)
         self.assertEquals(["a", "b"], list(entries))
+
+
+class DrevisionsTests(TestCaseWithTransport):
+
+    def setUp(self):
+        super(DrevisionsTests, self).setUp()
+        self.tree = self.make_branch_and_tree('.')
+        self.mapping = default_mapping
+        self.overlay = get_overlay(self.tree.branch.repository, self.mapping)
+
+    def drevs(self, revids, files, changelog_ids, manifest_ids, fileids={}, lossy=True):
+        return drevisions(self.tree.branch.repository, self.mapping,
+            revids, files, changelog_ids, manifest_ids,
+            self.overlay, fileids, lossy)
+
+    def test_empty(self):
+        self.assertEquals([], list(self.drevs([], {}, {}, {})))
